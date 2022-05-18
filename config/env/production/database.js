a6241b653ceca064bfd1504c1fa14480c52df2a0
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-05-18 16:27:17 -0400

    database conf

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,16 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password
-      },
-      debug: false,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: true, // For self-signed certificates,
     },
-  };
-};
+    debug: false,
+  },
+});
+
