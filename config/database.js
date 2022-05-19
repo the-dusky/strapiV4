f0316e136b4e7849925420bb3776bd2f6a5bc4fc
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-05-19 12:28:24 -0400

    ssl fix attempt

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: env('DATABASE_SSL', true), // For self-signed certificates,
+      ssl: { "rejectUnauthorized": env('DATABASE_SSL', true) } // For self-signed certificates,
     },
     debug: false,
   },
