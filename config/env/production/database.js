01cbb164d3d03c95d1ce6bcbf90d459de17e55de
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-07-06 13:40:55 -0400

    trying ssl false

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       schema: env('DATABASE_SCHEMA'), // Not required
-      ssl: { "rejectUnauthorized": env('DATABASE_SSL', false) } // For self-signed certificates,
+      ssl: false //{ "rejectUnauthorized": env('DATABASE_SSL', false) } // For self-signed certificates,
     },
     debug: false,
   },
