c19dfa62bafb3d3dde501c6562697ebdb0bc1908
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-07-06 14:15:39 -0400

    deleted page-theme

--- src/api/page-theme/routes/page-theme.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-theme router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::page-theme.page-theme');
