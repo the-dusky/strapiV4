4ec67d7cf3a97c77bb1fb242121c18ffc3be6354
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-08-05 09:05:19 -0400

    updated db development conf

--- config/database.js
+++ config/database.js
@@ -2,13 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
       schema: env('DATABASE_SCHEMA', 'v4'), // Not required
-      ssl: { rejectUnauthorized: env.bool('DATABASE_SSL', true), },
+      user: env('DATABASE_USERNAME', 'strapi'),
+      host: env('DATABASE_LOCATION') == 'local' ? env('LOCAL_DATABASE_HOST') : env('DATABASE_HOST'),
+      database: env('DATABASE_LOCATION') == 'local' ? env('LOCAL_DATABASE_NAME') : env('DATABASE_NAME'),
+      password: env('DATABASE_LOCATION') == 'local' ? env('LOCAL_DATABASE_PASSWORD') : env('DATABASE_PASSWORD'),
+      ssl: env('DATABASE_LOCATION') == 'local' ? env.bool('LOCAL_DATABASE_SSL') : { rejectUnauthorized: env.bool('DATABASE_SSL') },
     },
     debug: false,
   },
