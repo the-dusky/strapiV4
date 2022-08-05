d4ec384da3ec1d9c88584963d8d2eb5c02ec0ca7
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-08-05 13:16:25 -0400

    merged middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,8 +8,8 @@ module.exports = [
         directives: {
           'connect-src': ["'self'", 'https:'],
           'script-src': ["'self'","'unsafe-eval'"],
-          'img-src': ["'self'", 'data:', 'blob:', 'ik.imagekit.io','dl.airtable.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'ik.imagekit.io','dl.airtable.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'ik.imagekit.io', 'dl.airtable.com', 'strapi-v4-4as6.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'ik.imagekit.io', 'dl.airtable.com', 'strapi-v4-4as6.onrender.com'],
           upgradeInsecureRequests: null,
         },
       },
