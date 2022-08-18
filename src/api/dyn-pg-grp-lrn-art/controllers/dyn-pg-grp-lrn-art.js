f63327982afdb1ee4b9b58437fb8eee225818e5d
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-08-18 18:01:28 -0400

    added page group and carousel

---
+++ src/api/dyn-pg-grp-lrn-art/controllers/dyn-pg-grp-lrn-art.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  dyn-pg-grp-lrn-art controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dyn-pg-grp-lrn-art.dyn-pg-grp-lrn-art');
---
+++ src/api/dyn-pg-grp-lrn-art/routes/dyn-pg-grp-lrn-art.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dyn-pg-grp-lrn-art router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dyn-pg-grp-lrn-art.dyn-pg-grp-lrn-art');
