bd757df7dfd1353a02c7e02f6091015d7cca05ab
gildardoSOD <100874861+gildardoSOD@users.noreply.github.com>
2022-11-11 15:13:15 -0800

    feat(SNW-7): Add Summit collection data (#1)

---
+++ src/api/summit/controllers/summit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * summit controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::summit.summit');
